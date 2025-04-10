afde8d69f2c36eec0367b35abc1688e5f6090c74
cuplyk <121808859+cuplyk@users.noreply.github.com>
2025-04-10 21:51:10 +0000

    add: authentification

--- src/core/models.py
+++ src/core/models.py
@@ -1,8 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
+class Post(models.Model):
+    pass
+    
